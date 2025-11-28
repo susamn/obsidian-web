db0673bde2f4247c3ff0be495202aeaff9185005
Supratim Samanta <supratim.iee23.ju2005@gmail.com>
2025-11-28 15:23:20 -0500

    Introduce theme-specific heading hierarchy colors using CSS variables for consistent styling across MarkdownHeading components.

--- web/src/components/markdown/MarkdownHeading.vue
+++ web/src/components/markdown/MarkdownHeading.vue
@@ -37,27 +37,33 @@ const headingTag = computed(() => `h${props.node.level}`)
 }
 
 h1.md-heading {
+  color: var(--md-h1-color, var(--md-heading-color));
   font-size: 2.15em;
   font-weight: 700;
 }
 
 h2.md-heading {
+  color: var(--md-h2-color, var(--md-heading-color));
   font-size: 1.75em;
 }
 
 h3.md-heading {
+  color: var(--md-h3-color, var(--md-heading-color));
   font-size: 1.4em;
 }
 
 h4.md-heading {
+  color: var(--md-h4-color, var(--md-heading-color));
   font-size: 1.2em;
 }
 
 h5.md-heading {
+  color: var(--md-h5-color, var(--md-heading-color));
   font-size: 1.05em;
 }
 
 h6.md-heading {
+  color: var(--md-h6-color, var(--md-heading-color));
   font-size: 1em;
 }
 </style>
